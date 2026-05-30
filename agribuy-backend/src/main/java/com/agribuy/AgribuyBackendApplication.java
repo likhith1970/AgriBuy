aae9e24c3997d169b5c5b9ad2690dbbdf62e04de
Likhith1970 <likhithr1970@gmail.com>
2026-05-30 14:43:02 +0530

    Added Spring Boot Backend

---
+++ agribuy-backend/src/main/java/com/agribuy/AgribuyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agribuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgribuyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgribuyBackendApplication.class, args);
+	}
+
+}
---
+++ agribuy-backend/src/test/java/com/agribuy/AgribuyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agribuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgribuyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
